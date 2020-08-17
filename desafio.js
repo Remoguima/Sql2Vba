56a8209dc1c4aa981f8382e7896f4ec1132801d5
Remoguima <matheusrguimaraes@gmail.com>
2020-08-17 20:24:52 -0300

    My First Commit (literally)

---
+++ desafio.js
@@ -0,0 +1,12 @@
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: "1293",
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+};
+
+function retorna() {
+    console.log("O usuario mora em " + endereco.cidade + " / " + endereco.uf + " no bairro " + endereco.bairro + " na rua: '" + endereco.rua + "' com nº " + endereco.numero )
+    
+}

b974dd6bf891883a8a029907dd6338f66a33938e
Remoguima <matheusrguimaraes@gmail.com>
2020-08-19 21:31:32 -0300

    formatter funtionality increased, bugs correction

--- scripts/format.js
+++ scripts/format.js
@@ -1,15 +1,17 @@
         // JavaScript code to change 
         // the content of <textarea> 
-        function changeContent() { 
-            var rawtext = document.querySelector('textarea#raw')
+    function changeContent(){
+        var rawtext = document.querySelector('textarea#raw') //getting the original query
+        var splittedImput = rawtext.value.split('\n'); //dividing the original query by row
+        var convertedText = 'CSQL = " " \n' //initializing the converted query into a clean VBA variable
 
-            if (rawtext.value.length === 0) {
-                document.getElementById('converted').innerText = ""
+        for (var i=0; i<splittedImput.length; i++){ //initializing the loop
+            var linha = splittedImput[i];
+            
+            convertedText =convertedText+ 'CSQL = CSQL & "  '+ linha + '  "\n' //putting the row content into the VBA var
 
-                
-            }
-            else{
-                document.getElementById('converted').innerText 
-                = 'CSQL = CSQL &  "  ' + rawtext.value + '  "'; 
-            }       
-    } 
+            document.getElementById('converted').value 
+                = convertedText; // "printing" the var into the output box
+
+        }
+    }

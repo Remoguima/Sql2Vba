9000e7fe8f556acfb748e93961b1fb050c02272a
Remoguima <matheusrguimaraes@gmail.com>
2020-08-30 22:34:38 -0300

    adding clear and copy buttons

--- scripts/format.js
+++ scripts/format.js
@@ -5,13 +5,38 @@
         var splittedImput = rawtext.value.split('\n'); //dividing the original query by row
         var convertedText = 'CSQL = " " \n' //initializing the converted query into a clean VBA variable
 
-        for (var i=0; i<splittedImput.length; i++){ //initializing the loop
-            var linha = splittedImput[i];
-            
-            convertedText =convertedText+ 'CSQL = CSQL & "  '+ linha + '  "\n' //putting the row content into the VBA var
-
-            document.getElementById('converted').value 
-                = convertedText; // "printing" the var into the output box
-
+        if (rawtext.value.length>0) {
+            for (var i=0; i<splittedImput.length; i++){ //initializing the loop
+                var linha = splittedImput[i];
+                
+                convertedText =convertedText+ 'CSQL = CSQL & "  '+ linha + '  "\n' //putting the row content into the VBA var
+    
+                document.getElementById('converted').value = convertedText; // "printing" the var into the output box
+    
+            }   
         }
     }
+
+    function clearContent(){
+        document.getElementById('raw').value = ""
+        document.getElementById('converted').value = ""
+        
+
+    }
+
+    function copyToClipboard(){
+        // Get the text field
+        var copyText = document.getElementById("converted");
+
+        // Select the text field
+        copyText.select();
+        
+        // Copy the text inside the text field
+        document.execCommand("copy");
+        
+        // Alert the copied text
+        alert("Copied to the clipboard.")
+
+    }
+
+
